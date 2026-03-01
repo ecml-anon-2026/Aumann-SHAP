3a4d5a15584baa21cfdc66a8612a675231f629e9
ecml-anon <264793356+ecml-anon-2026@users.noreply.github.com>
2026-03-01 17:32:47 +0000

    Add explain() with grid_state or mc backend

--- src/aumann_shap/__init__.py
+++ src/aumann_shap/__init__.py
@@ -1,4 +1,6 @@
-__all__ = ["explain_tabular_gridstate"]
 __version__ = "0.0.0"
 
 from .tabular_gridstate import explain_tabular_gridstate
+from .explain import explain
+
+__all__ = ["explain", "explain_tabular_gridstate"]
